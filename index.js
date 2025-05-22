197780593a564265774edf6ce3ca987060129dd3
adi <adivekariya0@gmail.com>
2025-05-22 10:09:50 +0530

    little changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/products', productRoutes);
-app.use('/uploads', express.static('public/uploads'));
+app.use('/uploads', express.static('/public/uploads'));
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/users', userRoutes);
